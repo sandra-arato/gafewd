02356978a6ff6c91ca1dbcc282203485aeeea716
Sandra Szenti <alexandraszenti@gmail.com>
2013-09-15 19:29:52 +1000

    page setup

---
+++ public/js/data.js
@@ -0,0 +1,157 @@
+var staff = [
+	{
+		id:"anna",
+		name:"Anna Heisenberg",
+		team:"Design",
+		role:"Junior modeller",
+		email:"anna@busybees.com",
+		phone:"304",
+		room:"creative"
+	},
+	{
+		id:"ben",
+		name:"Ben Feynman",
+		team:"Production",
+		role:"CNC operator",
+		email:"ben@busybees.com",
+		phone:"602",
+		room:"proto"
+	},
+	{
+		id:"connor",
+		name:"Connor Bolyai",
+		team:"Production",
+		role:"RPT technologist",
+		email:"connor@busybees.com",
+		phone:"602",
+		room:"proto"
+	},
+	{
+		id:"diana",
+		name:"Diana Kepler",
+		team:"Management",
+		role:"Chief of Finance",
+		email:"diana@busybees.com",
+		phone:"107",
+		room:"office"
+	},
+	{
+		id:"eva",
+		name:"Eva Fibonacci",
+		team:"Management",
+		role:"Marketing Guru",
+		email:"eva@busybees.com",
+		phone:"201",
+		room:"office"
+	},
+	{
+		id:"fiona",
+		name:"Fiona Planck",
+		team:"Facilitation",
+		role:"Sr. Project Manager",
+		email:"fiona@busybees.com",
+		phone:"202",
+		room:"creative"
+	},
+	{
+		id:"gina",
+		name:"Gina Fourier",
+		team:"Office Support",
+		role:"Receptionist",
+		email:"gina@busybees.com",
+		phone:"101",
+		room:"reception"
+	},
+	{
+		id:"henry",
+		name:"Henry Hamilton",
+		team:"Management",
+		role:"CEO",
+		email:"henry@busybees.com",
+		phone:"108",
+		room:"office"
+	},
+	{
+		id:"ian",
+		name:"Ian Faraday",
+		team:"Facilitation",
+		role:"Project Manager",
+		email:"ian@busybees.com",
+		phone:"204",
+		room:"creative"
+	},
+	{
+		id:"jon",
+		name:"Jon Lorentz",
+		team:"Design",
+		role:"Product Designer",
+		email:"jon@busybees.com",
+		phone:"302",
+		room:"creative"
+	},
+	{
+		id:"kyle",
+		name:"Kyle Lobachevsky",
+		team:"Office Support",
+		role:"Handyman",
+		email:"kyle@busybees.com",
+		phone:"606",
+		room:"proto"
+	}
+]
+
+var rooms = [
+	{
+		room: "proto",
+		title: "Prototyping Lab",
+		about: 
+			"This is where the magic happens. Come in and check out the latest 3D printing technology which helps to deliver design concepts to all of our partners. Life would be a lot harder without these great machines and of course the people who are making it happen.",
+		workingarea: true,
+		workers: ""
+	},
+
+	{
+		room: "office",
+		title: "Office Space",
+		about: 
+			"Our very busy and very important Management team usually occupies this room for meetings or just to get their chores done. Whenever they're out of the office, you can sneak in if you need a quiet zone for your project or just some time alone.",
+		workingarea: true,
+		workers: ""
+	},
+
+	{
+		room: "reception",
+		title: "Reception",
+		about: 
+			"First impression counts. Because we truly believe in this, we made sure that the Reception tells the whole story of our company and represents the values that we stand for.",
+		workingarea: true,
+		workers: ""
+	},
+
+	{
+		room: "creative",
+		title: "Creative Block",
+		about: 
+			"Every new projects starts here, at the heart of the office. Facilitators and designers work together in this spacious room to sort out the craziest and most amazing solutions for our partners' problems. We move around our office furniture for each project to stimulate co-working through the process.",
+		workingarea: true,
+		workers: ""
+	},
+	{
+		room: "kitchen",
+		title: "Kitchen",
+		about: 
+			"The kitchen is everyone's favourite especially on a Friday afternoon. The fruit ball is reloaded every Monday, and beer is chilled in the fridge when we have to work longer hours.",
+		workingarea: false,
+		workers: ""
+	},
+
+	{
+		room: "lounge",
+		title: "Lounge Room",
+		about: 
+			"The Lounge is a great place for short naps, quick meetings and anything in between. When you feel like tinkering a bit, go ahead and make something in the electro corner. There is plenty of interesting books about design, prototyping and project managment on the shelf to, if you're a book worm.",
+		workingarea: false,
+		workers: ""
+	}
+
+]
---
+++ public/js/main.js
@@ -0,0 +1,96 @@
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+
+function sendEmailAddress () {
+
+	var emailAddress = $("input").val();
+
+	if( !isValidEmailAddress(emailAddress) ) {
+		alert("Hmm, looks like there's something wrong with that email. Try again!")
+	}
+	else {
+		alert("Cool! Now go and check your inbox for the first issue ;)");
+	}
+}
+
+function faqHandler (e) {
+	var answerShow = $("#faq-answer-display").html();
+	if (answerShow == "hide all") {
+		$("#faq-site h3 + p").slideUp();
+		$("#faq-site h3").css("line-height", "0.4");
+		$("#faq-answer-display").html("show all");
+	}
+	else {
+		$("#faq-site h3 + p").slideDown();
+		$("#faq-answer-display").html("hide all");
+	}
+
+}
+
+function renderStaffPhotos (page){
+	var div = $(".team");
+
+	for (var index in staff) {
+		var Div = $("<div />", {
+    		id: staff[index].id,
+    		class: "element " + staff[index][page]
+			});
+		var photo = $("<div />")
+			.html(staff[index].name)
+			.addClass("photo")
+			.css("background-image", "url(images/"+staff[index].id+".jpg)");
+		var name = $("<p />").html(staff[index].name);
+		var team = $("<p />").html(staff[index].team);
+		$(Div).append(photo, name, team);
+		$(div).append(Div); 
+	}
+}
+
+function filterStaff () {
+	var selector = $(this).attr("data-filter");
+  	$(".team").isotope({ filter: selector });
+  	return false;
+}
+
+function lookForRoom (roomDivTitle) {
+	for (var index in rooms) {
+		if (roomDivTitle == rooms[index].room) {
+			var currentRoom = rooms[index];
+			return currentRoom;
+		}
+	}
+}
+
+function changeRoom() {
+	var roomDivTitle = $(this).attr("id").split("-")[0];
+	var currentRoom = lookForRoom(roomDivTitle);
+
+	$("#room-description h2").html(currentRoom.title);
+	$("#room-description span + p").html(currentRoom.about);
+	$("#room-description .click-text").css("display", "none");
+
+}
+
+$(document).ready(function() {
+	
+	$("#get-email-address").click(sendEmailAddress);
+
+	$("body#faq-site #faq-answer-display").click(faqHandler);
+
+	var pageId = $("body").attr("id");
+	if (pageId == "workspace-site") {
+		renderStaffPhotos("room");
+	$(".team").isotope({ filter: ".none" });
+	}
+	else if (pageId == "team-site") {
+		renderStaffPhotos("team");
+	}
+
+	$("body#team-site #company-structure a").click(filterStaff); 										
+	
+	$("body#workspace-site #map div").click(changeRoom).click(filterStaff);
+
+});
